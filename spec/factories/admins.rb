ecb4b529d93656e8bfbed61f5b52bd8b5b9c6d23
stephan-dev <stephan-dev@outlook.fr>
2019-01-24 18:18:06 +0100

    test admin model

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -2,6 +2,5 @@
 
 FactoryBot.define do
   factory :admin do
-    email { Faker::Internet.email }
   end
 end
--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -3,11 +3,5 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-describe 'Model instantiation' do
-    subject(:new_admin) { described_class.new }
-
-    describe 'Database' do
-      it { is_expected.to have_db_column(:email).of_type(:string).with_options(null: false, default: "") }
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
