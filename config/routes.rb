dd9d077d64e90542c0f56d2e58586d4694f39b42
stephan-dev <stephan-dev@outlook.fr>
2019-01-25 17:06:15 +0100

    root route and auto-created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'items#index'
+  get 'items/index'
+  get 'items/show'
   devise_for :admins
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
