419406b44838493e40fa6a958d4be1d4f317c010
stephan-dev <stephan-dev@outlook.fr>
2019-01-25 17:02:44 +0100

    annotate models

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: wishlists
+#
+#  id         :bigint(8)        not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Wishlist < ApplicationRecord
   has_many :item_wishlists, dependent: :destroy
   has_many :items, through: :item_wishlists
