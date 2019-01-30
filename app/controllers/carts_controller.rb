5d1756b07c548017ce4489aec43dd1add142ca5f
Crysicia <lucienmontmartre@yahoo.fr>
2019-01-30 17:35:27 +0100

    Fix parameters overlapping

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -9,7 +9,12 @@ class CartsController < ApplicationController
   def update
     item = params[:item].to_i
     quantity = params[:quantity].to_i
-    @cart.find_or_create_by(item: item).increment(:quantity, quantity).save
+
+    if params[:type].downcase == "increment"
+      @cart.find_or_create_by(item: item).increment(:quantity, quantity).save
+    else
+      @cart.find_or_create_by(item: item).decrement(:quantity, quantity).save
+    end
   end
 
   def destroy
