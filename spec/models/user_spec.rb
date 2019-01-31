d45d8f793f2ded8c4890d40aec6db32c1b4ba6a4
Crysicia <lucienmontmartre@yahoo.fr>
2019-01-31 17:50:11 +0100

    Add tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -40,6 +40,11 @@ RSpec.describe User, type: :model do
       it { is_expected.to have_db_column(:confirmation_sent_at).of_type(:datetime) }
       it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
       it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
+      it { is_expected.to have_db_column(:sign_in_count).of_type(:integer).with_options(null: false, default: 0) }
+      it { is_expected.to have_db_column(:current_sign_in_at).of_type(:datetime) }
+      it { is_expected.to have_db_column(:last_sign_in_at).of_type(:datetime) }
+      it { is_expected.to have_db_column(:current_sign_in_ip).of_type(:inet) }
+      it { is_expected.to have_db_column(:last_sign_in_ip).of_type(:inet) }
     end
 
     describe 'Associations' do
