e3e9225592b00d731515f9d17bc8edd13667ceb2
Yin Yué <yyue87@gmail.com>
2019-02-06 17:18:07 +0100

    add test validations

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 class Stock < ApplicationRecord
-  validates :quantity, presence: true
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal: 0 }
   belongs_to :item
 end
--- spec/models/stock_spec.rb
+++ spec/models/stock_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Stock, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Model instantiation' do
+    subject(:new_stock) { described_class.new }
+
+    it { is_expected.to have_db_column(:id).of_type(:integer).with_options(null: false) }
+    it { is_expected.to have_db_column(:quantity).of_type(:integer).with_options(presence:true, numericality: { only_integer: true, greater_than_or_equal: 0 }) }
+    it { is_expected.to have_db_column(:item_id).of_type(:integer) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
+  end
+
+  describe 'Associations' do
+    it { is_expected.to belong_to(:item) }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of(:quantity) }
+  end
 end
