40a5bd020ae4c62078a44bf243a6191bb8536f0d
stephan-dev <stephan-dev@outlook.fr>
2019-02-06 12:21:52 +0100

    restore condition in admin item controller

--- app/controllers/administration/items_controller.rb
+++ app/controllers/administration/items_controller.rb
@@ -18,9 +18,13 @@ module Administration
 
     def create
       @item = Item.new(item_params)
-      Item.create(item_params)
-      flash[:notice] = "Item was successfully created!"
-      redirect_to administration_items_path
+      if @item.save
+        flash[:notice] = "L'article a été créé"
+        redirect_to administration_items_path
+      else
+        redirect_to administration_items_path
+        flash[:notice] = "Il y a eu problème, veuillez vérifier les champs"
+      end
     end
 
     def edit
@@ -30,9 +34,13 @@ module Administration
     def update
       @item = Item.find(params[:id])
 
-      @item.update(item_params)
-      flash[:notice] = "Item was successfully updated!"
-      redirect_to administration_items_path
+      if @item.update(item_params)
+        flash[:notice] = "L'article a bien été modifié"
+        redirect_to administration_items_path
+      else
+        redirect_to administration_items_path
+        flash[:notice] = "Il y a eu problème, veuillez vérifier les champs"
+      end
     end
 
     def destroy
